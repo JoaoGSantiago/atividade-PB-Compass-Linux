f4152b825247ba4304b446f286741eddeafc11b0
EC2 Default User <ec2-user@ip-172-31-43-97.ec2.internal>
2024-12-18 17:34:28 +0000

    script e arquivos de monitoramento

---
+++ script_status_nginx.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Variáveis
+servico="nginx"
+# Variável para armazenar o dia, data, hora e segundo
+tempo=$(date '+%Y-%m-%d %H:%M:%S')
+
+status_online=~/monitoracao_online.log
+status_offline=~/monitoracao_offline.log
+
+
+# Verifica o status do serviço
+if systemctl is-active --quiet $servico; then
+    echo "$tempo - $servico - ONLINE - Serviço está online." >> $status_online
+else
+    echo "$tempo - $servico - OFFLINE - Serviço está parado." >> $status_offline
+fi
